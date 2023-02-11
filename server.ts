369dd11826009c55aac4be018e0daabf650df003
Agu Chux <agu.chux@yahoo.com>
2023-02-11 09:38:59 +0100

    Server up

--- server.ts
+++ server.ts
@@ -1,13 +1,10 @@
 import { config } from "dotenv";
 config();
 
-// import cluster from "node:cluster";
-// import * as os from "os";
-// const totalCPUs = os.cpus().length;
 
 import express, { Express } from "express";
 import cors from "cors";
-import helmet from "helmet";
+// import helmet from "helmet";
 
 import schoolsRouter from "./routers/schools";
 import homeRouter from "./routers/home";
@@ -18,51 +15,23 @@ import lecturersRouter from "./routers/lecturers";
 import studentsRouter from "./routers/students";
 import openaiRouter from "./routers/metrics";
 
-// if (cluster.isPrimary) {
-//   console.log(`Primary ${process.pid} is running`);
-//   // Fork workers.
-//   for (let i = 0; i < totalCPUs; i++) {
-//     cluster.fork();
-//   }
-//   cluster.on("listening", (address: object) => {
-//     // Worker is listening
-//     console.log(`Worker is listening on ${address}`);
-//   });
-//   cluster.on("exit", (worker, code: number, signal: string) => {
-//     if (signal) {
-//       console.log(`worker was killed by signal: ${signal}`);
-//     } else if (code !== 0) {
-//       console.log(`worker exited with error code: ${code}`);
-//     } else {
-//       console.log("worker success!");
-//     }
-//     console.log("Forking a new Worker.....");
-//     cluster.fork();
-//   });
-//   cluster.on("disconnect", () => {
-//     // Worker has disconnected
-//     console.log("Worker has disconnected");
-//   });
-// } else {
-// }
-
 const server: Express = express();
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-const allowedOrigins: string[] = [
-  "https://api.metrics.ng",
-  "https://app.metrics.ng",
-  "https://esut.metrics.ng",
-  "https://owner.metrics.ng",
-  "https://metrics.ng",
-  "http://localhost:4200",
-];
+// const allowedOrigins: string[] = [
+//   "https://api.metrics.ng",
+//   "https://app.metrics.ng",
+//   "https://esut.metrics.ng",
+//   "https://owner.metrics.ng",
+//   "https://metrics.ng",
+//   "http://localhost:4200",
+// ];
 
 server.use(
   cors({
     optionsSuccessStatus: 200,
-    origin: allowedOrigins,
+    origin: "https://esut.metrics.ng",
     methods: ["GET", "POST"],
   })
 );
@@ -76,9 +45,8 @@ server.use("/students", studentsRouter);
 server.use("/metrics", openaiRouter);
 server.use("/", homeRouter);
 
-
-
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Running: http://localhost:${port}`);
 });
+
